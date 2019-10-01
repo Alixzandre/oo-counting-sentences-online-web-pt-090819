4279b48b91703efe93a9fbb2fd5e74d632d272be
Alexander Bispo <alexbispo173@gmail.com>
2019-10-01 12:32:04 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -27,7 +27,8 @@ class String
   end
 
   def count_sentences
-      count = 0
+      new_array = self.split(/[.?!]/).reject {|string| string.empty?}
+    return new_array.length
       
   end
 end
